e055fa903e25d4fe8f68bff02a8ca7998005c8c1
andyzg <zhangandy94@gmail.com>
2014-01-24 18:33:12 -0500

    Added trackpad fragment

---
+++ gen/com/andyzg/droidpad/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.andyzg.droidpad;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ gen/com/andyzg/droidpad/TrackpadFragment.java
@@ -0,0 +1,15 @@
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TrackpadFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+    	
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_main, container, false);
+    }
+}
