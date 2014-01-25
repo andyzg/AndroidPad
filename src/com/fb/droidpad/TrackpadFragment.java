2a4522f00fa469cf5768f7ca7b520bb9d578ab20
andyzg <zhangandy94@gmail.com>
2014-01-25 02:11:22 -0500

    Message formatting

--- src/com/fb/droidpad/TrackpadFragment.java
+++ src/com/fb/droidpad/TrackpadFragment.java
@@ -6,6 +6,8 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.UUID;
 
+import org.json.JSONObject;
+
 import android.app.Fragment;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothSocket;
@@ -23,13 +25,15 @@ public class TrackpadFragment extends Fragment implements ServerSocketListener {
 	private OutputStream mOutputStream = null;
 	private PrintWriter mPrintWriter = null;
 	private BluetoothSocket mSocket = null;
+	private BluetoothAdapter mBluetoothAdapter = null;
 	
 	private Button mButton1;
 	
 	private static final String TAG = "Trackpad Fragment";
-	private String MAC = "";
-	private BluetoothAdapter mBluetoothAdapter = null;
+	
+	
 	private static final String NAME = "Droidpad";
+	private static final String MSG_HEADER = "DROIDPAD 1.0";
 	
 	private UUID mUUID = UUID.fromString("370a07a0-8557-11e3-9d72-0002a5d5c51b");
 	
@@ -39,6 +43,21 @@ public class TrackpadFragment extends Fragment implements ServerSocketListener {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		// If BT is not on, request that it be enabled.
+	        // setupCommand() will then be called during onActivityResult
+		if (!mBluetoothAdapter.isEnabled()) {
+			Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+			
+			ensureDiscoverable();
+		}
+		// otherwise set up the command service
+		else {
+			Log.d(TAG, "Starting Asynctask");
+			new ServerTask(mUUID, NAME, this).execute(mBluetoothAdapter);
+			// new ClientTask(mUUID, MAC, this).execute(mBluetoothAdapter);
+		}
 	}
 
 	@Override
@@ -61,22 +80,7 @@ public class TrackpadFragment extends Fragment implements ServerSocketListener {
 	@Override
 	public void onStart() {
 		super.onStart();
-		
-		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		// If BT is not on, request that it be enabled.
-	        // setupCommand() will then be called during onActivityResult
-		if (!mBluetoothAdapter.isEnabled()) {
-			Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-			startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
-			
-			ensureDiscoverable();
-		}
-		// otherwise set up the command service
-		else {
-			Log.d(TAG, "Starting Asynctask");
-			new ServerTask(mUUID, NAME, this).execute(mBluetoothAdapter);
-			// new ClientTask(mUUID, MAC, this).execute(mBluetoothAdapter);
-		}
+
 	}
 
 	@Override
@@ -97,9 +101,14 @@ public class TrackpadFragment extends Fragment implements ServerSocketListener {
 	public void sendMessage() {
 		if (mPrintWriter != null) {
 			Log.d(TAG, "Sending hello world!");
-			mPrintWriter.println("Hello world");
+			mPrintWriter.print("Hello world");
 			mPrintWriter.flush();
 		}
+		
+		String action = createAction(x, y);
+		mPrintWriter.append(MSG_HEADER);
+		mPrintWriter.println(action);
+		mPrintWriter.flush();
 	}
 	
     private void ensureDiscoverable() {
